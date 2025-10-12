c69d0fd7f4f1ecd401dcc7595910032a4f488ecc
Asaad Ali <asaad.ali.dev@gmail.com>
2025-10-12 21:55:49 +0300

    allow specifying custom collection name

--- services/db.js
+++ services/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 var db
 async function connect() {
     try {
-        await mongoose.connect(`mongodb+srv://${process.env.DBUSERNAME}:${process.env.DBPASSWORD}@${process.env.DB_HOST}/?retryWrites=true&w=majority&appName=${process.env.DB_APP_NAME}`)
+        await mongoose.connect(`mongodb+srv://${process.env.DBUSERNAME}:${process.env.DBPASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME ? process.env.DB_NAME : "" }?retryWrites=true&w=majority&appName=${process.env.DB_APP_NAME}`)
         db = mongoose.connection
         console.log('MongoDB connected successfully')
     }
