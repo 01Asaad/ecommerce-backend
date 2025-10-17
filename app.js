b7451d112616d2b7b6903f05b392592040a3e230
Asaad Ali <asaad.ali.dev@gmail.com>
2025-10-17 13:25:58 +0300

    added rate limit for production environments

--- app.js
+++ app.js
@@ -9,13 +9,17 @@ import testRoutes from "./routes/testRoutes.js"
 import cors from "cors"
 import path from "path"
 import {sleep} from "./util/utils.js"
+
 import { errorHandler, notFound } from './middleware/error.js';
 import { singleImageMiddleware } from './middleware/contentTypeManagers.js';
+import limiter from './middleware/rateLimit.js';
+
 config()
 const app = express();
 app.use(cors({
     origin: process.env.ALLOWED_ORIGINS.split(',') || 'http://localhost:3000'
 }));
+
 
 app.use(express.json())
 app.use(singleImageMiddleware)
@@ -25,13 +29,17 @@ const __dirname = path.resolve()
 app.use("/api/", generalRoutes)
 app.use("/api/auth", authRoutes)
 app.use("/api/products", productRoutes)
-if (process.env.ENV === "development") {
-    console.log("running with test routes enabled");
-    app.use("/api/test", testRoutes)
-}
 // app.use("/api/orders", orderRoutes)
 
 app.use("/images", express.static(path.join(__dirname, "images")))
+
+if (process.env.ENV === "development") {
+    console.log("running with test routes enabled");
+    app.use("/api/test", testRoutes)
+} else {
+    app.use(limiter); 
+}
+
 
 app.use(notFound)
 app.use(errorHandler)
